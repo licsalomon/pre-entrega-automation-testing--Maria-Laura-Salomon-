a8f4e8db10fe7712b7628bb86c207f62b7c050ce
licsalomon <liclaurasalomon@gmail.com>
2025-10-23 19:54:24 -0300

    Inclusion de los archivos  utils-conftest-runTest, organizacion de las carpetas y completado test login y catalogo

---
+++ tests/test_catalogo.py
@@ -0,0 +1,35 @@
+
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+def test_catalogo(login_in_driver):
+    try:
+     #login
+        driver = login_in_driver 
+
+     #Título de la página
+        assert driver.title == "Swag Labs", "El titulo no es el correcto"
+ 
+     #Verificar productos visibles
+        productos = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+        print(f'Se encontraron {len(productos)} productos.')
+
+      #Capturar nombre y precio del primer producto
+        primerNombre  = productos[0].find_element(By.CSS_SELECTOR, ".inventory_item_name").text
+        primerPrecio = productos[0].find_element(By.CSS_SELECTOR, ".inventory_item_price").text
+        print(f"Primer producto: {primerNombre} – {primerPrecio}")
+
+     #Verificar filtro
+        driver.find_element(By.CLASS_NAME,'product_sort_container').click()
+        time.sleep(2)
+
+     #Verificar menu
+        driver.find_element(By.CLASS_NAME,'bm-burger-button').click()
+        time.sleep(2)
+        linkMenu = driver.find_element(By.ID, 'inventory_sidebar_link')
+        assert linkMenu.is_displayed(), "Menu no encontrado"
+        driver.find_element(By.CLASS_NAME,'bm-cross-button').click
+    
+    finally:
+        driver.quit()
---
+++ tests/test_login.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import conftest
+import pytest
+
+def test_login_validation(login_in_driver):
+    try:
+     #login
+        driver = login_in_driver
+    
+
+     #validación de la redirección de la página
+        assert "/inventory.html" in driver.current_url, "No se redirgio al inventario"
+        
+     #Tratamiento de errores
+    except Exception as e:
+        print(f"Error en test_login: {e}")
+        raise
+    finally:
+        driver.quit()
+        
+
+
+
+    
+    
